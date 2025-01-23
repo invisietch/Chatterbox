a2b71c719cba86e793d804f592856713dd44e5a4
invisietch <realechelon@proton.me>
2025-01-23 01:29:22 +0000

    Improve navbar.

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -2,32 +2,40 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import SettingsBox from './SettingsBox';
 
+import { ChatAlt2Icon, TagIcon, UserGroupIcon, UserCircleIcon, LightBulbIcon, ArrowCircleUpIcon, AdjustmentsIcon } from '@heroicons/react/outline';
+
 const Navbar = () => {
   const router = useRouter();
 
-  const navItems = [
-    { href: '/', label: 'Conversations' },
-    { href: '/tags', label: 'Tags' },
-    { href: '/characters', label: 'Characters' },
-    { href: '/personas', label: 'Personas' },
-    { href: '/prompts', label: 'Prompts' },
-    { href: '/import-export', label: 'Import/Export' },
-    { href: '/llm', label: 'Presets' },
-  ];
-
   return (
-    <nav className="bg-dark1 h-16 flex items-center px-6">
+    <nav className="bg-dark1 h-16 flex items-center px-6 sticky top-0 z-50">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center space-x-8">
-          <div className="text-xl font-bold text-white">
+          <div className="text-3xl font-bold text-white">
             <Link href="/">Chatterbox</Link>
           </div>
-          <div className="flex space-x-4">
-            {navItems.map((item) => (
-              <Link key={item.href} href={item.href}>
-                {item.label}
-              </Link>
-            ))}
+          <div className="flex space-x-4 text-grey-300 hover:text-grey-100">
+            <Link key='/' href='/' className={`${router.pathname === '/' ? 'text-grey-300' : 'text-brightYellow'}`}>
+              <ChatAlt2Icon className='h-8 w-8' />
+            </Link>
+            <Link key='/tags' href='/tags' className={`${router.pathname === '/tags' ? 'text-grey-300' : 'text-brightYellow'}`}>
+              <TagIcon className='h-8 w-8' />
+            </Link>
+            <Link key='/characters' href='/characters' className={`${router.pathname === '/characters' ? 'text-grey-300' : 'text-brightYellow'}`}>
+              <UserGroupIcon className='h-8 w-8' />
+            </Link>
+            <Link key='/pesonas' href='/personas' className={`${router.pathname === '/personas' ? 'text-grey-300' : 'text-brightYellow'}`}>
+              <UserCircleIcon className='h-8 w-8' />
+            </Link>
+            <Link key='/prompts' href='/prompts' className={`${router.pathname === '/prompts' ? 'text-grey-300' : 'text-brightYellow'}`}>
+              <LightBulbIcon className='h-8 w-8' />
+            </Link>
+            <Link key='/import-export' href='/import-export' className={`${router.pathname === '/import-export' ? 'text-grey-300' : 'text-brightYellow'}`}>
+              <ArrowCircleUpIcon className='h-8 w-8' />
+            </Link>
+            <Link key='/llm' href='/llm' className={`${router.pathname === '/llm' ? 'text-grey-300' : 'text-brightYellow'}`}>
+              <AdjustmentsIcon className='h-8 w-8' />
+            </Link>
           </div>
         </div>
 
